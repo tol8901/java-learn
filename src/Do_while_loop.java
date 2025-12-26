1fcfb150c984b1b81a8efcb7e18b25fc1e93e377
Anatolii Kravchenko <tol890@ukr.net>
2025-12-27 00:14:11 +0100

    Do While loop

--- src/Do_while_loop.java
+++ src/Do_while_loop.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 public class Do_while_loop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter 5");
-        int value = scanner.nextInt();
-        while(value != 5) {
+        int value;
+        do {
             System.out.println("Enter 5");
             value = scanner.nextInt();
-        }
+        } while(value != 5);
         System.out.println("You entered 5");
     }
 }
