4b199dcbb6d5ee33e1eb993161da3afa83ae1224
Anatolii Kravchenko <tol890@ukr.net>
2025-12-28 00:35:22 +0100

    Continue operator

---
+++ src/Break_Continue.java
@@ -0,0 +1,21 @@
+public class Break_Continue {
+    public static void main(String[] args) {
+        int i = 0;
+        while(true) {
+            if (i == 15) {
+                break;
+            }
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("We are out of the loop.");
+
+
+        for (int j = 0; j <= 15; j++) {
+            if (j % 2 == 0) {
+                continue;
+            }
+            System.out.println("This is an odd number " + j);
+        }
+    }
+}
