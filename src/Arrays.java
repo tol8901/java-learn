b46b78a692a44afe919dbc02eb5e3fa17279dbe5
Anatolii Kravchenko <tol890@ukr.net>
2025-12-28 20:39:59 +0100

    Arrays of numbers

--- src/Arrays.java
+++ src/Arrays.java
@@ -2,6 +2,18 @@ public class Arrays {
     public static void main(String[] args) {
         int number = 10;
         int[] numbers = new int[5];
-        System.out.println(numbers[3]);
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i * 10;
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Array element: " + numbers[i]);
+        }
+
+        int[] numbers1 = {1, 2, 3, 4, 5};
+        for (int i = 0; i < numbers1.length; i++) {
+            System.out.println("Numbers1 array element: " + numbers1[i]);
+        }
     }
 }
