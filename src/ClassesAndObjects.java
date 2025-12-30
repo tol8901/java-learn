4a4813e0a84578e922924ffe05820d81493994eb
Anatolii Kravchenko <tol890@ukr.net>
2025-12-30 12:54:28 +0100

    Classes: methods

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -3,16 +3,26 @@ public class ClassesAndObjects {
         Person person1 = new Person();
         person1.name = "Roman";
         person1.age = 50;
-        System.out.println("My name is " + person1.name + " and I am " + person1.age);
+        person1.sayHello();
 
         Person person2 = new Person();
         person2.name = "Vova";
         person2.age = 20;
-        System.out.println("My name is " + person2.name + " and I am " + person2.age);
+        person2.speak();
     }
 }
 
 class Person {
     String name;
     int age;
+
+    void speak() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("My name is " + name + " I am " + age + " years old.");
+        }
+    }
+
+    void sayHello() {
+        System.out.println("Hello!");
+    }
 }
