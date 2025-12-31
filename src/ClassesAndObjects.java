6537c49ab73aef6c86ab768d78600045717a2836
Anatolii Kravchenko <tol890@ukr.net>
2025-12-31 11:06:31 +0100

    Classes: methods parameters

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,22 +1,26 @@
 public class ClassesAndObjects {
     public static void main(String[] args) {
         Person person1 = new Person();
-        person1.name = "Roman";
-        person1.age = 50;
+        person1.setNameAndAge("Roman", 20);
         int year1 = person1.calculateYearsToRetirement();
-        System.out.println("First person has to retirement: " + year1 + " years");
+        person1.speak();
 
         Person person2 = new Person();
-        person2.name = "Vova";
-        person2.age = 20;
+        String s1 = "Vova";
+        person2.setNameAndAge(s1, 30);
         int year2 = person2.calculateYearsToRetirement();
-        System.out.println("Second person has to retirement: " + year2  + " years");
+        person2.speak();
     }
 }
 
 class Person {
     String name;
     int age;
+
+    void setNameAndAge(String username, int userage) {
+        name = username;
+        age = userage;
+    }
 
     int calculateYearsToRetirement() {
         int years = 65 - age;
