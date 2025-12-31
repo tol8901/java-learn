ca16828ae784e95e212e4b13df5e83af5e17313b
Anatolii Kravchenko <tol890@ukr.net>
2025-12-31 10:34:00 +0100

    Classes: methods and returned values

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -3,18 +3,25 @@ public class ClassesAndObjects {
         Person person1 = new Person();
         person1.name = "Roman";
         person1.age = 50;
-        person1.sayHello();
+        int year1 = person1.calculateYearsToRetirement();
+        System.out.println("First person has to retirement: " + year1 + " years");
 
         Person person2 = new Person();
         person2.name = "Vova";
         person2.age = 20;
-        person2.speak();
+        int year2 = person2.calculateYearsToRetirement();
+        System.out.println("Second person has to retirement: " + year2  + " years");
     }
 }
 
 class Person {
     String name;
     int age;
+
+    int calculateYearsToRetirement() {
+        int years = 65 - age;
+        return years;
+    }
 
     void speak() {
         for (int i = 0; i < 3; i++) {
@@ -25,4 +32,6 @@ class Person {
     void sayHello() {
         System.out.println("Hello!");
     }
+
+
 }
