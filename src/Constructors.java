03b43d403ac99d27a1f6e91d038825f49a3645bd
Anatolii Kravchenko <tol890@ukr.net>
2026-01-02 19:40:55 +0100

    Classes: the static keyword

--- src/Constructors.java
+++ src/Constructors.java
@@ -1,17 +1,14 @@
 public class Constructors {
     public static void main(String[] args) {
-
         Human h1 = new Human("Bob", 40);
-        Human h2 = new Human("Tom", 30)
-        Human.description = "Nice";
-        h1.getAllFields();
-        h2.getAllFields();
-        Human.description = "Bad";
-        h1.getAllFields();
-        h2.getAllFields();
+        Human h2 = new Human("Tom", 30);
+        h1.pritnNumberOfPeopele();
+        h2.pritnNumberOfPeopele();
+        Human h3 = new Human("Rob", 50);
+        h1.pritnNumberOfPeopele();
+        h2.pritnNumberOfPeopele();
+        h3.pritnNumberOfPeopele();
 
-        System.out.println(Math.pow(2, 4));
-        System.out.println(Math.PI);
 
     }
 }
@@ -19,12 +16,13 @@ public class Constructors {
 class Human {
     private String name;
     private int age;
-    public static String description;
+    private static int countPeople;
 
     public Human(String name, int age) {
         System.out.println("Hello from third constructor!");
         this.name = name;
         this.age = age;
+        countPeople++;
     }
 
     public void setName(String name) {
@@ -34,15 +32,7 @@ class Human {
         this.age = age;
     }
 
-    public static void getDescription() {
-        System.out.println(description);
+    public void pritnNumberOfPeopele() {
+        System.out.println("Number of people is: " + countPeople);
     }
-
-    public void getAllFields() {
-        System.out.println(name + ", " + age + ", " + description);
-    }
-
-    public static void printAllFields() {
-        System.out.println();
-    }
-}
+ }
