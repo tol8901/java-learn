183cb6ca8e21c931f2944b304f3cad320423689a
Anatolii Kravchenko <tol890@ukr.net>
2026-01-05 19:07:24 +0100

    Classes: the static keyword

---
+++ src/Lesson22FinalKeyword.java
@@ -0,0 +1,10 @@
+public class Lesson22FinalKeyword {
+    public static void main(String[] args) {
+        final int X = 10;
+        System.out.println(X);
+    }
+}
+
+class Test {
+    public final int CONSTANT = 0;
+}
