4bd3aa889d28b3b69df9445e856e2b6ea0b29bba
Anatolii Kravchenko <tol890@ukr.net>
2026-01-06 09:48:26 +0100

    StringBuilder: part 1

---
+++ src/Lesson23.java
@@ -0,0 +1,11 @@
+public class Lesson23 {
+    public static void main(String[] args) {
+        String string1 = "Hello";
+        String string2 = " my";
+        String string3 = " friend";
+        String stringAll = string1 + string2 + string3;
+        System.out.println(stringAll);
+
+
+    }
+}
