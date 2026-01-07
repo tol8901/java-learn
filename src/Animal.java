41e8ae29d99f4adbec37c0fbb5648c2bce7f8bdc
Anatolii Kravchenko <tol890@ukr.net>
2026-01-07 12:08:19 +0100

    Inheritance

---
+++ src/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+
+    String name = "Some animal";
+
+    public void eat() {
+        System.out.println("Animal is eating");
+    }
+
+    public void sleep() {
+        System.out.println("Animal is sleeping");
+    }
+}
