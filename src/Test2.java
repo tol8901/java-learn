154b7b65ec62eadea2f41fb8db5165eb6c87ba6a
Anatolii Kravchenko <tol890@ukr.net>
2026-01-14 09:47:44 +0100

    Access modifiers: part 1

---
+++ src/Test2.java
@@ -0,0 +1,16 @@
+public class Test2 {
+    public int id;
+    private int id2;
+
+    public  static void main(String[] args) {
+        // public, private default protected
+    }
+
+    private void tehcnicalMethod() {}
+
+    int id3; //default (available inside the package, but available external for the subclasses)
+}
+
+class Test3{
+
+}
